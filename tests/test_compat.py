0fc72362b7202f8c4ba97eff94c928f3c21b9daa
Anders Madsen <anders.madsen@dagrofa.dk>
2025-05-02 07:47:26 +0200

    Test update

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -23,28 +23,28 @@ class Test(TestCase):
             self.assertEqual(result, expected_result)
 
 
-    # def test_get_relative_path(self):
-    #     if platform == "win32":
-    #         tests = {
-    #             "sql\\file.sql" : ("sql\\file.sql", "sql\\file.sql"),
-    #             "sql\\file.sql" : ("sql\\file.sql", "c:\\app\\"),
-    #             "sql\\file.sql" : ("c:\\app\\sql\\file.sql", "c:\\app\\"),
-    #         }
+    def test_get_relative_path(self):
+        if platform == "win32":
+            tests = {
+                "sql\\file.sql" : ("sql\\file.sql", "sql\\file.sql"),
+                "sql\\file.sql" : ("sql\\file.sql", "c:\\app\\"),
+                "sql\\file.sql" : ("c:\\app\\sql\\file.sql", "c:\\app\\"),
+            }
 
-    #         p = "sql\\file.sql"
-    #         self.assertEqual(p, get_relative_path(p, p))
-    #     else:
-    #         tests = {
-    #             "sql/file.sql" : ("sql/file.sql", "sql/file.sql"),
-    #             "sql/file.sql" : ("sql/file.sql", "/app/"),
-    #             "sql/file.sql" : ("/app/sql/file.sql", "/app/"),
-    #         }
-    #         p = "sql/file.sql"
-    #         self.assertEqual(p, get_relative_path(p, p))
+            p = "sql\\file.sql"
+            self.assertEqual(p, get_relative_path(p, p))
+        else:
+            tests = {
+                "sql/file.sql" : ("sql/file.sql", "sql/file.sql"),
+                "sql/file.sql" : ("sql/file.sql", "/app/"),
+                "sql/file.sql" : ("/app/sql/file.sql", "/app/"),
+            }
+            p = "sql/file.sql"
+            self.assertEqual(p, get_relative_path(p, p))
 
-    #     for expected_result, (file_path, root) in tests.items():
-    #         result = get_relative_path(file_path, root)
-    #         self.assertEqual(result, expected_result)
+        for expected_result, (file_path, root) in tests.items():
+            result = get_relative_path(file_path, root)
+            self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':
